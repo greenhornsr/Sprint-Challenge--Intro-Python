0b65817b3fa1119f2c3612375c435396356a4938
greenhornsr <srollins11279@gmail.com>
2020-01-17 09:37:03 -0800

    ran tests...updated a few comp code blocks to get them to pass.

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -54,19 +54,20 @@ print(e)
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = tuple([(human.name, human.age) for human in humans if human.age > 27 and human.age < 32])
+f = [tuple((human.name, human.age)) for human in humans if human.age > 27 and human.age <= 32]
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = [(human.name.upper(), human.age + 5) for human in humans]
+g = [Human(human.name.upper(), human.age + 5)for human in humans]
 print(g)
+
 
 # Write a list comprehension that contains the square root of all the ages.
 ####  I ALSO used round to shrink the output length to a more readable length.
 print("Square root of ages:")
 import math
-h = [round(math.sqrt(human.age), 2) for human in humans]
+h = [math.sqrt(human.age) for human in humans]
 print(h)
