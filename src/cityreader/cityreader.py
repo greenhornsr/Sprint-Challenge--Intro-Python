988ef517ad26e8a2b6b9f65b8ce24f95161f7595
greenhornsr <srollins11279@gmail.com>
2020-01-17 10:15:34 -0800

    added escape n to print out

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -8,7 +8,7 @@ class City:
     self.lon = lon
   
   def __str__(self):
-    return f"{self.name}, {self.lat}, {self.lon}"
+    return f"{self.name}, {self.lat}, {self.lon}\n"
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
