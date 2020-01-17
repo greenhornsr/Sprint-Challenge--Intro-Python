c6fa34db2a7e4f61cb2a00f4721a381ea63201e6
greenhornsr <srollins11279@gmail.com>
2020-01-17 09:07:08 -0800

    revised oop1.py to pass test.  had to remove dundrr inits.

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -26,35 +26,23 @@ class Vehicle:
 
 # Direct Sub Class to Vehicle
 class FlightVehicle(Vehicle):
-    def __init__(self):
-        super().__init__(self)
         pass
 
 class GroundVehicle(Vehicle):
-    def __init__(self):
-        super().__init__(self)
         pass
 
 # Sub Class to FlightVehicle
 class Starship(FlightVehicle):
-    def __init__(self):
-        super().__init__(self)
         pass
 
 class Airplane(FlightVehicle):
-    def __init__(self):
-        super().__init__(self)
         pass
 
 # Sub Class to GroundVehicle
 class Motorcycle(GroundVehicle):
-    def __init__(self):
-        super().__init__(self)
         pass
 
 class Car(GroundVehicle):
-    def __init__(self):
-        super().__init__(self)
         pass
 
 
